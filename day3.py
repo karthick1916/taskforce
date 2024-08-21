fdff3544baab9040eda026344155432b41b0c742
karthick1916 <tkarthickat@gmail.com>
2024-08-21 20:37:50 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,2 +1,12 @@
 import streamlit as st
+import google.generativeai as genai
 st.title("My title")
+genai.configure(api_key="AIzaSyBaPZ-p3WH7Lrli-QkDe7LXHJOR8yY-8pQ")
+text = input()
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+if st.button("click me"):
+    response = chat.send_message(text)
+    st.write(response.text)
