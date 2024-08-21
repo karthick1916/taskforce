65e8ef5f34bf42d76572f13f8367609ed4bdd869
karthick1916 <tkarthickat@gmail.com>
2024-08-21 21:02:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import google.generativeai as genai
 st.title("My title")
 genai.configure(api_key="AIzaSyBaPZ-p3WH7Lrli-QkDe7LXHJOR8yY-8pQ")
-text = input()
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
